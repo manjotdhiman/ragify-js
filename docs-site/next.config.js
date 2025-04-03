224c395c99691123136af4b2d16d4c23e6c46b9d
Manjot Singh <manjotdhiman@gmail.com>
2025-04-03 13:46:02 +0400

    fix: docx

--- docs-site/next.config.js
+++ docs-site/next.config.js
@@ -1,11 +1,12 @@
 import nextra from 'nextra'
  
 const withNextra = nextra({
-  
+
   // ... Other Nextra config options
 })
  
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
+  output: 'export',
   // ... Other Next.js config options
 })
